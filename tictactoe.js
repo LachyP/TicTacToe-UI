c8084e20d2005f7fd221d1be1803c364d220caf0
LachyP <lachiep@live.com.au>
2021-07-04 15:09:33 +0930

    values displayed alternate between max and min, system can also load values from a string to update the playing board"

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,26 @@
-var boardState = "ooxoxx---";
-
+var sampleState = "ooxoxx---";
+var maxsturn = true;
 function insertChar(elem){
-    document.getElementsByClassName("cell")[elem].innerHTML="x";
+    var char;
+    if(maxsturn){
+        char = "x";
+        maxsturn = false;
+    }
+    else{
+        char = "o";
+        maxsturn = true;
+    }
+    document.getElementsByClassName("cell")[elem].innerHTML=char;
+
+    //loadBoardState(sampleState);
+}
+
+function loadBoardState(state){
+    //state is 9 characters, there are 9 cells in the table
+    //go through each character, update the corresponding cell
+    for(var i = 0; i < 9; i++){
+        if(state[i] != '-') {
+            document.getElementsByClassName("cell")[i].innerHTML = state[i];
+        }
+    }
 }
