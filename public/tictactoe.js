61b7a77ce42c48e6730b2280db019c3c363045c4
LachyP <lachiep@live.com.au>
2021-07-05 15:43:08 +0930

    no longer allows for players moves to override moves that have already been taken

--- public/tictactoe.js
+++ public/tictactoe.js
@@ -1,25 +1,27 @@
 var sampleState = "ooxoxx---";
 var maxsturn = true;
 function insertChar(elem){
-    var char;
-    if(maxsturn){
-        char = "x";
-        maxsturn = false;
-        document.getElementById("whoseturn").innerHTML="Player 2's turn";
+    //don't let player's moves override moves that have already been taken
+    if(document.getElementsByClassName("cell")[elem].innerHTML === "") {
+        var char;
+        if (maxsturn) {
+            char = "x";
+            maxsturn = false;
+            document.getElementById("whoseturn").innerHTML = "Player 2's turn";
+        } else {
+            char = "o";
+            maxsturn = true;
+            document.getElementById("whoseturn").innerHTML = "Player 1's turn";
+        }
+        document.getElementsByClassName("cell")[elem].innerHTML = char;
     }
-    else{
-        char = "o";
-        maxsturn = true;
-        document.getElementById("whoseturn").innerHTML="Player 1's turn";
-    }
-    document.getElementsByClassName("cell")[elem].innerHTML=char;
-
     //loadBoardState(sampleState);
 }
 
 function loadBoardState(state){
     //state is 9 characters, there are 9 cells in the table
     //go through each character, update the corresponding cell
+    //will be needed for reading in c++ program output from backend
     for(var i = 0; i < 9; i++){
         if(state[i] != '-') {
             document.getElementsByClassName("cell")[i].innerHTML = state[i];
@@ -32,6 +34,10 @@ function resetBoard(){
     maxsturn = true;
 
     for(var i = 0; i < 9; i++){
-        document.getElementsByClassName("cell")[i].innerHTML=" ";
+        document.getElementsByClassName("cell")[i].innerHTML="";
     }
+}
+
+function isGameOver(){
+
 }
