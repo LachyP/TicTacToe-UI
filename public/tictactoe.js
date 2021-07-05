b84b2c9383243b5f7bf743c181de1e0c4cf0f688
LachyP <lachiep@live.com.au>
2021-07-05 16:16:47 +0930

    program successfully determines terminal states, it is playable by 2 local users, need to implement ai player

--- public/tictactoe.js
+++ public/tictactoe.js
@@ -1,14 +1,15 @@
 var sampleState = "ooxoxx---";
 var maxsturn = true;
 var boardState = "---------"
+var moveCounter = 0;
 
 var gameOverDisplayed = false;
 function insertChar(elem){
     //if the game over screen is displayed, then reset the board with another click
     if(gameOverDisplayed){
         resetBoard();
-        resetBoard();
         gameOverDisplayed = false;
+        moveCounter = 0;
     }
     else {
         //don't let player's moves override moves that have already been taken
@@ -24,7 +25,7 @@ function insertChar(elem){
                 document.getElementById("whoseturn").innerHTML = "Player 1's turn";
             }
             document.getElementsByClassName("cell")[elem].innerHTML = char;
-            //
+            moveCounter++;
             boardState = boardState.split('');
 
             boardState.splice(elem,1,char);
@@ -37,7 +38,11 @@ function insertChar(elem){
                 gameOverDisplayed = true;
             }
             if (isGameOver() === "o") {
-                document.getElementById("whoseturn").innerHTML = "Game Over - Player 2 Wins!"
+                document.getElementById("whoseturn").innerHTML = "Game Over - Player 2 Wins!";
+                gameOverDisplayed = true;
+            }
+            if(isGameOver() === "draw") {
+                document.getElementById("whoseturn").innerHTML = "Game Over - Draw";
                 gameOverDisplayed = true;
             }
 
@@ -52,7 +57,7 @@ function loadBoardState(state){
     //go through each character, update the corresponding cell
     //will be needed for reading in c++ program output from backend
     for(var i = 0; i < 9; i++){
-        if(state[i] != '-') {
+        if(state[i] !== '-') {
             document.getElementsByClassName("cell")[i].innerHTML = state[i];
         }
     }
@@ -68,12 +73,41 @@ function resetBoard(){
 }
 
 function isGameOver(){
-    if(document.getElementsByClassName("cell")[5].innerHTML === "x"){
-        return "x";
+    //check if isMax, if true, then min's turn was last, check if min has won,
+    //otherwise check if max has won
+    if((boardState[0] === boardState[1] && boardState[1] === boardState[2] && boardState[2] !== '-')
+        || (boardState[3] === boardState[4] && boardState[4] === boardState[5] && boardState[5] !== '-')
+        || (boardState[6] === boardState[7] && boardState[7] === boardState[8] && boardState[8] !== '-')){
+        if(maxsturn){
+            return "o";
+        }
+        else{
+            return "x";
+        }
     }
-    if(document.getElementsByClassName("cell")[5].innerHTML === "o"){
-        return "o";
+
+    if((boardState[0] === boardState[3] && boardState[3] === boardState[6] && boardState[6] !== '-')
+        || (boardState[1] === boardState[4] && boardState[4] === boardState[7] && boardState[7] !== '-')
+        || (boardState[2] === boardState[5] && boardState[5] === boardState[8] && boardState[8] !== '-')){
+        if(maxsturn){
+            return "o";
+        }
+        else{
+            return "x";
+        }
+    }
+    if((boardState[0] === boardState[4] && boardState[4] === boardState[8] && boardState[8] !== '-')
+        || (boardState[6] === boardState[4] && boardState[4] === boardState[2] && boardState[2] !== '-')){
+        if(maxsturn){
+            return "o";
+        }
+        else{
+            return "x";
+        }
+    }
+
+    if(moveCounter === 9){
+        return "draw";
     }
     return "-";
-
 }
