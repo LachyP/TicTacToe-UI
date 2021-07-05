8a049ed9ca33d2297cb1dd8c9baad63282665a11
LachyP <lachiep@live.com.au>
2021-07-05 15:34:46 +0930

    tidied up index.js backend file

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 import Express from "express";
-//console.log('hello world');
-
-//const app = Express();
 const port = 3000;
-// GET PUT POST DELETE
-
-//let express = import('express');
 
 const app = Express()
 
 app.get('/', function(req,res){res.sendFile("/users/lachi/Documents/Personal Projects/TicTacToe-UI/public/tictactoe.html")})
 app.use(Express.static('/users/lachi/Documents/Personal Projects/TicTacToe-UI/public'))
 app.listen(port, () => console.log("listening on port " + port))
-/*app.get("/", (req,res) => {
-    res.sendFile("/users/lachi/Documents/Personal Projects/TicTacToe-UI/public/tictactoe.html");
-});
-app.use(express.static(__dirname + '/public'))
-
-app.listen(port, ()=>console.log("listening on port " + port));
-*/
