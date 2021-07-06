56e59e1a5ff689a60f1199b69d8ef9ebe375efb7
LachyP <lachiep@live.com.au>
2021-07-06 15:14:25 +0930

    the game is playable with randomly generated ai moves through the receive request function. Now just need to change the recieve request function so that it recieves the c++ minimax output code from the server side of this application

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const port = 3000;
 const app = Express();
 
 app.get('/', function(req,res){
+    //this is the only page in the app, only need to send a text file.
     res.sendFile("/users/lachi/Documents/Personal Projects/TicTacToe-UI/public/tictactoe.html")
 });
+
 app.use(Express.static('/users/lachi/Documents/Personal Projects/TicTacToe-UI/public'));
 app.listen(port, () => console.log("listening on port " + port));
 
--- public/tictactoe.js
+++ public/tictactoe.js
@@ -4,6 +4,33 @@ var boardState = "---------"
 var moveCounter = 0;
 
 var gameOverDisplayed = false;
+
+function generateRandomInteger(maxVal){
+    return Math.floor(Math.random() * maxVal) ;
+}
+
+function recieveRequest() {
+    //document.getElementById("whoseturn").innerHTML = "CPU's turn";
+    let validNumberGenerated = false;
+    let randomNumber = 0;
+   // console.log("yes");
+    while (!validNumberGenerated) {
+        //console.log("enter function");
+        randomNumber = generateRandomInteger(9);
+        if(boardState[randomNumber] === '-'){
+            console.log(randomNumber);
+            validNumberGenerated = true;
+        }
+    }
+    validNumberGenerated = false;
+    //once random free space has been found on the board, place character there
+    boardState = boardState.split('');
+
+    boardState.splice(randomNumber, 1, 'o');
+    console.log(boardState.join(''));
+    return boardState.join('');
+}
+
 function insertChar(elem){
     //if the game over screen is displayed, then reset the board with another click
     if(gameOverDisplayed){
@@ -12,7 +39,60 @@ function insertChar(elem){
         moveCounter = 0;
     }
     else {
+        //x's turn
+        if (document.getElementsByClassName("cell")[elem].innerHTML === "") {
+            let char = "x";
+
+            //players move
+            document.getElementsByClassName("cell")[elem].innerHTML = char;
+            moveCounter++;
+            boardState = boardState.split('');
+
+            boardState.splice(elem, 1, char);
+            let newState = boardState.join('');
+            boardState = newState;
+            console.log(boardState);
+
+            //game over check 1
+            maxsturn = false;
+            if (isGameOver() === "x") {
+                document.getElementById("whoseturn").innerHTML = "Congrats, you won!";
+                gameOverDisplayed = true;
+            }
+            if (isGameOver() === "draw") {
+                document.getElementById("whoseturn").innerHTML = "Its a draw";
+                gameOverDisplayed = true;
+            }
+
+            //ai's move
+            if(!gameOverDisplayed) {
+                console.log("ai's move");
+                char = "o";
+                document.getElementById("whoseturn").innerHTML = "AI's turn";
+                boardState = recieveRequest();
+                //wait a few seconds, simulate thinking
+                setTimeout(function () {
+                    loadBoardState(boardState);
+                    document.getElementById("whoseturn").innerHTML = "Your turn";
+                    maxsturn = true;
+                    //game over check 2
+                    if (isGameOver() === "o") {
+                        document.getElementById("whoseturn").innerHTML = "AI wins";
+                        gameOverDisplayed = true;
+                    }
+                }, 2000);
+
+            }
+        }
+    }
+    //check if game over
+
+    //if not then o's turn (ai)
+
+    //check if game over
+    /*else {
         //don't let player's moves override moves that have already been taken
+
         if (document.getElementsByClassName("cell")[elem].innerHTML === "") {
             var char;
             if (maxsturn) {
@@ -23,6 +103,7 @@ function insertChar(elem){
                 char = "o";
                 maxsturn = true;
                 document.getElementById("whoseturn").innerHTML = "Player 1's turn";
+                //recieveRequest();
             }
             document.getElementsByClassName("cell")[elem].innerHTML = char;
             moveCounter++;
@@ -48,8 +129,9 @@ function insertChar(elem){
 
 
         }
-    }
-    //loadBoardState(sampleState);
+    }*/
+    //boardState = recieveRequest(elem);
+    //loadBoardState(boardState);
 }
 
 function loadBoardState(state){
@@ -75,6 +157,7 @@ function resetBoard(){
 function isGameOver(){
     //check if isMax, if true, then min's turn was last, check if min has won,
     //otherwise check if max has won
+    //console.log("function executing");
     if((boardState[0] === boardState[1] && boardState[1] === boardState[2] && boardState[2] !== '-')
         || (boardState[3] === boardState[4] && boardState[4] === boardState[5] && boardState[5] !== '-')
         || (boardState[6] === boardState[7] && boardState[7] === boardState[8] && boardState[8] !== '-')){
@@ -82,6 +165,7 @@ function isGameOver(){
             return "o";
         }
         else{
+
             return "x";
         }
     }
