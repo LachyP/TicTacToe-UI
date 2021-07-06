96be80781b78b252ef2a5f91bce7dfbf260756be
LachyP <lachiep@live.com.au>
2021-07-06 13:57:01 +0930

    just changed some syntax stuff

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import Express from "express";
 const port = 3000;
 
-const app = Express()
+const app = Express();
 
-app.get('/', function(req,res){res.sendFile("/users/lachi/Documents/Personal Projects/TicTacToe-UI/public/tictactoe.html")})
-app.use(Express.static('/users/lachi/Documents/Personal Projects/TicTacToe-UI/public'))
-app.listen(port, () => console.log("listening on port " + port))
+app.get('/', function(req,res){
+    res.sendFile("/users/lachi/Documents/Personal Projects/TicTacToe-UI/public/tictactoe.html")
+});
+app.use(Express.static('/users/lachi/Documents/Personal Projects/TicTacToe-UI/public'));
+app.listen(port, () => console.log("listening on port " + port));
+
